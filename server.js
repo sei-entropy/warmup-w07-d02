6036e8fc064cb7d700be504b55fce3e6535f9e41
malsaadan <mmalsaadan@gmail.com>
2020-03-02 09:45:19 +0300

    solution

--- server.js
+++ server.js
@@ -1,19 +1,66 @@
-// require express
-// const express = require('express')
+// Loading the express module on our server
+const express = require("express");
 
-// for this example, we'll use an in-memory array in place of a database
+// Creates a new instance of express on our server
+const app = express();
+
+// ********   Middleware      ******
+// parse JSON request sent by the user
+// and converts it into JS object before
+// a route uses it
+app.use(express.json());
+
 const books = [
-    { title: 'Dictionary', author: 'Webster' }, // 0
-    { title: 'Encyclopedia', author: 'Encarta' }, // 1
-    { title: 'Clean Code', author: 'Robert Cecil Martin' } // 2
-  ]
-  
-  // create an Express app object
-  
-  // GET /books returns all books
-  
-  // GET /books/:id returns single book
-  
-  // DELETE /books/:id deletes a book
-  
-  // launch server
+  { title: "Dictionary", author: "Webster" }, // 0
+  { title: "Encyclopedia", author: "Encarta" }, // 1
+  { title: "Clean Code", author: "Robert Cecil Martin" } // 2
+];
+
+// for calling the API
+app.get("/api/library", function(req, res) {
+  res.json({
+    library: books
+  });
+});
+
+app.get('/api/library/:id' , function (req , res) { 
+    const bookID = req.params.id
+     const book = books[bookID]; 
+     console.log(book);
+    res.json({ 
+        library: book, 
+    }); 
+    });
+
+app.post("/api/library", function(req, res) {
+  books.push(req.body.book);
+  res.status(201).json({
+    books: books
+  });
+});
+
+//delete  person
+app.delete("/api/library/:id", function(req, res) {
+  const bookId = req.params.id;
+  books.splice(bookId, 1);
+  res.send("got a delete  requst ");
+});
+
+// update person
+app.put('/api/library/:id', function(req, res){
+    const bookID = req.params.id;
+    books[bookID] = req.body.book;
+    res.json({
+        books: books,
+    })
+});
+
+
+// tells the server where to listen for requests
+const port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  // tells the server where to listen for requests on port 3000
+
+  console.log(`hello-express is listening on port ${port}`);
+}); // actualizing the line above
