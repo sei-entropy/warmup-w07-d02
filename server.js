934aeb1d4cf5aca0cabd9fef29c7ebbf63c20e12
afnankb <afnankbasudan@gmail.com>
2020-03-02 09:20:15 +0300

    done

--- server.js
+++ server.js
@@ -1,19 +1,101 @@
-// require express
-// const express = require('express')
 
-// for this example, we'll use an in-memory array in place of a database
+
+// Loading the express module on our server
+const express = require('express');
+
+// Creates a new instance of express on our server
+const app = express();
+
+
+
+
+// tells the server where to listen for requests
+const port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  // tells the server where to listen for requests on port 3000
+
+  console.log(`hello-express is listening on port ${port}`);
+}); // actualizing the line above
+
+
+
 const books = [
     { title: 'Dictionary', author: 'Webster' }, // 0
     { title: 'Encyclopedia', author: 'Encarta' }, // 1
     { title: 'Clean Code', author: 'Robert Cecil Martin' } // 2
   ]
   
-  // create an Express app object
+
+
+app.get('/', function(req ,res){
+   
+    res.send("hello world ")
+    
+    });
+
+
+    app.get('/books', function(req,res){
+
+
+        res.json({
+            // key   value 
+            books : books
+        })
   
-  // GET /books returns all books
   
-  // GET /books/:id returns single book
-  
-  // DELETE /books/:id deletes a book
-  
-  // launch server
+       })
+
+
+
+
+       app.get('/books/:id', function(req,res){
+      
+        const bookId = req.params.id;
+        const book = people[bookId];
+
+        if (book !== undefined) {
+            res.json({ books: books });
+          } else {
+            res.status(404).json({ error: 'books Not Found'});
+          }
+
+
+       })
+
+
+       app.post('/books', function(req,res){
+      
+        books.push(req.body.book)
+          res.status(201).json({
+            books : books
+          })
+ 
+ 
+        })
+
+        //update person 
+        app.put('/books/:id', function(req,res){
+      
+        const bookId = req.params.id;
+        books[bookId] = req.body.book
+          res.send("got a put requst ")
+
+        })
+
+
+
+         //delete  person 
+         app.delete('/books/:id', function(req,res){
+      
+        const bookId = req.params.id;
+        books.splice(bookId ,1)
+          res.send("got a delete  requst ")
+
+        })
+
+
+
+
+
+
